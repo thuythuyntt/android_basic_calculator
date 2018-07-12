2313119ee472c616aed2649a697d3fb4510a8c63
THUY <thuthuynt.0710@gmail.com>
2018-07-12 10:12:44 +0700

    first commit

--- app/src/main/java/ntt/thuy/practice/framgia/androidbasiccalculator/MainActivity.java
+++ app/src/main/java/ntt/thuy/practice/framgia/androidbasiccalculator/MainActivity.java
@@ -8,5 +8,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializationView();
+    }
+
+    private void initializationView() {
     }
 }
