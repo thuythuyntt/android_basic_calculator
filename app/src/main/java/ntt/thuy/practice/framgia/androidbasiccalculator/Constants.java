2383ad44404d3c74ee78ca614bd1049dafab94b9
THUY <thuthuynt.0710@gmail.com>
2018-07-13 08:19:54 +0700

    add function_project

---
+++ app/src/main/java/ntt/thuy/practice/framgia/androidbasiccalculator/Constants.java
@@ -0,0 +1,12 @@
+package ntt.thuy.practice.framgia.androidbasiccalculator;
+
+/**
+ * Created by thuy on 12/07/2018.
+ */
+public class Constants {
+    public static final String DIVISION_STRING = "/";
+    public static final String MULTIPLICATION_STRING = "x";
+    public static final String SUBTRACTION_STRING = "-";
+    public static final String SUMMATION_STRING = "+";
+    public static final String DOT_STRING = ".";
+}
